3428f8185e4c0f3042201389bed9d25ac7cac95a
Abel Trotter and Nikki Boyd <abel@>
2018-06-04 15:39:54 -0700

    Add entry box for user input, convert input to lowercase, count words

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+function countUniqueWords(array) {
+  array.forEach(function(word) {
+    var counter = [];
+    array.forEach(function(word2) {
+      if (word === word2) counter.push(word);
+    });
+    $("#results").append("<li>" + word + ": " + counter.length + "</li>");
+  });
+}
+
+function lowerCaseWords(input) {
+  var words = input.split(" ");
+  var lowerCase = words.map(function(item) {
+    return item.toLowerCase();
+  });
+  return lowerCase;
+}
+
+$(document).ready(function() {
+  $("#submit").click(function(e) {
+    e.preventDefault();
+
+    var input = $("#inputWords").val();
+    var newInput = lowerCaseWords(input);
+    countUniqueWords(newInput);
+  });
+});
+
+// Type type type stuff stuff hey hi hello
