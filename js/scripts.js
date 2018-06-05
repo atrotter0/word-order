ca214f1afa402a64ce0001904040065acb3de0eb
Abel Trotter <a_trotter0@hotmail.com>
2018-06-04 18:47:25 -0700

    Add class on submit, rmeove old data from DOM, add logic to remove dupes

--- js/scripts.js
+++ js/scripts.js
@@ -6,27 +6,33 @@ function lowerCaseWords(input) {
   return lowercaseList;
 }
 
-function countUniqueWords(inputArray) {
+function countAndDisplay(inputArray) {
   inputArray.forEach(function(word) {
     var counter = [];
     inputArray.forEach(function(word2) {
       if (word === word2) counter.push(word);
     });
-    filterAndDisplay();
-    //$("#results").append("<li class='list-item'>" + word + ": " + counter.length + "</li>");
+    displayWord(counter, word);
   });
 }
 
-function filterAndDisplay() {
+function displayWord(counter, word) {
+  var wordAndCount = word + ":" + counter.length;
+  var values = $("li").text().split(" ");
 
+  if (!values.includes(wordAndCount)) {
+    $("#results").append("<li class='list-item'>" + word + ":" + counter.length + " </li>");
+  }
 }
 
 $(document).ready(function() {
   $("#submit").click(function(e) {
     e.preventDefault();
 
+    $("li").remove();
+    $(".well").addClass("fancy-box");
     var input = $("#inputWords").val();
     var lowercaseInput = lowerCaseWords(input);
-    countUniqueWords(lowercaseInput);
+    countAndDisplay(lowercaseInput);
   });
 });
